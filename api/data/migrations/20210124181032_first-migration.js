f605db7c93697b6cf33b34795c3752726fcc8ff1
Douglas Little <douglas.a.little@gmail.com>
2021-05-21 09:02:28 -0400

    Add migration

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,14 +1,68 @@
 exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
+      users.increments('userId')
+      users.string('firstName').notNullable()
+      users.string('lastName').notNullable()
+      users.string('email').notNullable()
+      users.string('password').notNullable()
+      users.boolean('isTrainer').notNullable().defaultTo(false)
       users.timestamps(false, true)
+    })
+
+  await knex.schema
+    .createTable('locations', (locations) => {
+      locations.increments('locationId')
+      locations.string('name').notNullable()
+      locations.string('address').notNullable()
+    })
+
+  await knex.schema
+    .createTable('classes', (classes) => {
+      classes.increments('classId')
+      classes.integer('trainerId')
+        .notNullable()
+        .references('userId')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      classes.string('type').notNullable()
+      classes.time('startTime').notNullable()
+      classes.date('date').notNullable()
+      classes.time('duration').notNullable()
+      classes.string('intensityLevel').notNullable()
+      classes.integer('location')
+        .notNullable()
+        .references('locationId')
+        .inTable('locations')
+        .onDelete('SET NULL')
+        .onUpdate('CASCADE')
+      classes.integer('currentRegistered').notNullable().defaultTo(0)
+      classes.integer('maxRegistered').notNullable()
+    })
+
+  await knex.schema
+    .createTable('client_classes', (c_c) => {
+      c_c.integer('clientId')
+        .notNullable()
+        .references('userId')
+        .inTable('users')
+        .onDelete('SET NULL')
+        .onUpdate('CASCADE')
+      c_c.integer('classId')
+        .notNullable()
+        .references('classId')
+        .inTable('classes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      c_c.date('signUpDate').notNullable().defaultTo(knex.raw('current_timestamp'))
+      c_c.primary(['clientId', "classId"])
     })
 }
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('client_classes')
+  await knex.schema.dropTableIfExists('classes')
+  await knex.schema.dropTableIfExists('locations')
   await knex.schema.dropTableIfExists('users')
 }
